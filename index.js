d60c7bd52d83d58a4bc5f089e724b0226749b402
sbeltier <sarahnbeltier@gmail.com>
2021-08-31 23:22:46 -0700

    updated

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: '$hru33eRy@667%',
+        password: '',
         database: 'employee_tracker',
     },
     console.log(`Connected to the database.`)
